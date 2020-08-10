a6eb316ef5b2cce73d36e083568b77f6e3f713c7
dayofgray <graysonpberger@gmail.com>
2020-08-10 08:23:24 -0400

    Add Login Form Component to Home Page

--- health-ramp-frontend/src/App.js
+++ health-ramp-frontend/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      currentUser: null,
+      loginForm: {
+        email: "",
+        password: ""
+      }
+    }
+  }
+
+  handleLoginFormChange = event => {
+    const { name, value} = event.target
+    this.setState({
+      loginForm: {
+        ...this.state.loginForm,
+        [name]: value
+      }
+    })
+  }
+
+  handleLoginFormSubmit = event => {
+      event.preventDefault()
+
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        Welcome {this.state.currentUser}
+        <Login handleLoginFormSubmit={this.handleLoginFormSubmit} handleLoginFormChange={this.handleLoginFormChange} email={this.state.loginForm.email} password={this.state.loginForm.password}/>
+      </div>
+    );
+  }
 }
 
 export default App;
