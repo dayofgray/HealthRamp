6ca9cdceb0aa91299c15b868b1e2bd1c9f698978
dayofgray <graysonpberger@gmail.com>
2020-08-15 23:57:53 -0400

    Add redux and thunk

---
+++ health-ramp-frontend/src/store.js
@@ -0,0 +1,13 @@
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import currentUser from './reducers/currentUser.js'
+
+const reducer = combineReducers({
+    currentUser
+  })
+  
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
