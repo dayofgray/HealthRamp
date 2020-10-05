14ab277628f370b8e1d2509747cf5ddd881e18ec
dayofgray <graysonpberger@gmail.com>
2020-10-05 11:03:30 -0400

    Show link to recipe steps

--- health-ramp-frontend/src/components/presentational/Recipe.js
+++ health-ramp-frontend/src/components/presentational/Recipe.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const Recipe = ({name, ingredients}) => {
+export const Recipe = ({name, ingredients, onClick, id}) => {
   const ingredientList = ingredients.map((i) => <li key={i.id}>{i.name}</li>)
 
   return (
-    <div class="recipe-tile">
+    <div class="recipe-tile" onClick={() => onClick(id)}>
       <h2>{name}</h2> <br></br>
       <p class="bold">Ingredients({ingredients.length})</p>
       <ul>
